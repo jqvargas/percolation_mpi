e78da96427e0121f4acfee5e66a96431e251a8c4
jqvargas <103585724+jqvargas@users.noreply.github.com>
2025-04-06 14:18:42 -0400

    modified slurms scripts

--- cw2/run-gpu-1.sh
+++ cw2/run-gpu-1.sh
@@ -3,6 +3,7 @@
 #SBATCH --qos=short
 #SBATCH --gres=gpu:1
 #SBATCH --time=00:05:00
+#SBATCH --account=mdisspt-s2266011
 
 module load gcc/10.2.0 nvidia/nvhpc-nompi/24.5 openmpi/4.1.6-cuda-12.4
 
@@ -13,12 +14,12 @@ module load compiler
 # OpenMP
 export OMP_TARGET_OFFLOAD=MANDATORY
 
-echo "Small test"
-srun --ntasks 1 build/test -s 987 -M 512 -N 512 -P 1 -Q 1 -o small-$SLURM_JOB_ID.png
+#echo "Small test"
+#srun --ntasks 1 build/test -s 987 -M 512 -N 512 -P 1 -Q 1 -o small-$SLURM_JOB_ID.png
 
-echo "Medium test"
+#echo "Medium test"
 # reduce porosity to reduce run time
-srun --ntasks 1 build/test -s 9876 -p 0.5 -M 1024 -N 2048 -P 1 -Q 1 -o medium-$SLURM_JOB_ID.png
+#srun --ntasks 1 build/test -s 9876 -p 0.5 -M 1024 -N 2048 -P 1 -Q 1 -o medium-$SLURM_JOB_ID.png
 
 echo "Large test"
-srun --ntasks 1 build/test -s 98765 -p 0.3 -M 4096 -N 4096 -P 1 -Q 1 -o large-$SLURM_JOB_ID.png
+srun --ntasks 1 src/build/test -s 98765 -p 0.3 -M 4096 -N 4096 -P 1 -Q 1 -o large-$SLURM_JOB_ID.png
--- cw2/run-gpu-8.sh
+++ cw2/run-gpu-8.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=2
 #SBATCH --exclusive
 #SBATCH --time=00:05:00
-
+#SBATCH --account=mdisspt-s2266011
 module load gcc/10.2.0 nvidia/nvhpc-nompi/24.5 openmpi/4.1.6-cuda-12.4
 
 # SYCL
@@ -15,12 +15,12 @@ module load compiler
 # OpenMP
 export OMP_TARGET_OFFLOAD=MANDATORY
 
-echo "Small test"
-srun --ntasks 8 --tasks-per-node=4 build/test -s 987 -M 512 -N 512 -P 2 -Q 4 -o small-$SLURM_JOB_ID.png
+#echo "Small test"
+#srun --ntasks 8 --tasks-per-node=4 build/test -s 987 -M 512 -N 512 -P 2 -Q 4 -o small-$SLURM_JOB_ID.png
 
-echo "Medium test"
+#echo "Medium test"
 # reduce porosity to reduce run time
-srun --ntasks 8 --tasks-per-node=4 build/test -s 9876 -p 0.5 -M 1024 -N 2048 -P 2 -Q 4 -o medium-$SLURM_JOB_ID.png
+#srun --ntasks 8 --tasks-per-node=4 build/test -s 9876 -p 0.5 -M 1024 -N 2048 -P 2 -Q 4 -o medium-$SLURM_JOB_ID.png
 
 echo "Large test"
-srun --ntasks 8 --tasks-per-node=4 build/test -s 98765 -p 0.3 -M 4096 -N 4096 -P 2 -Q 4 -o large-$SLURM_JOB_ID.png
+srun --ntasks 8 --tasks-per-node=4 src/build/test -s 98765 -p 0.3 -M 4096 -N 4096 -P 2 -Q 4 -o large-$SLURM_JOB_ID.png
